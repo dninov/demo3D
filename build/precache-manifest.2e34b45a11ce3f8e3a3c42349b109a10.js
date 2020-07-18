251a90b26447f14bd649245e3d5f818ebe5baea6
unknown <dninov@gmail.com>
2020-07-18 12:44:19 +0300

    up

---
+++ build/precache-manifest.2e34b45a11ce3f8e3a3c42349b109a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490c6ddcb966886fa62d2bb7b3220f89",
+    "url": "/demo3D/index.html"
+  },
+  {
+    "revision": "0b902a00b590f0c05dcf",
+    "url": "/demo3D/static/css/main.4f44a739.chunk.css"
+  },
+  {
+    "revision": "88236fcb557d7b3bac8a",
+    "url": "/demo3D/static/js/2.946e4ea4.chunk.js"
+  },
+  {
+    "revision": "dcf3a185959f9a17c684adf4f0f19bd1",
+    "url": "/demo3D/static/js/2.946e4ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b902a00b590f0c05dcf",
+    "url": "/demo3D/static/js/main.4c97b51c.chunk.js"
+  },
+  {
+    "revision": "a8056e8a7da963b3a2ac",
+    "url": "/demo3D/static/js/runtime-main.bb69d188.js"
+  }
+]);
